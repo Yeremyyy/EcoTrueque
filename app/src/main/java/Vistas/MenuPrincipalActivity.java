c053aaf8db52df2b389b2714f92ede022aa52fd8
Yeremyyy <zackusskgg@gmail.com>
2026-09-05 10:18:44 -0400

    Quinto commit: Se corrigio el radiogroup, ya que dejaba fuera algunas categorias. Por otro lado, se implemento una accion de click en el MenuActivity, falta contectar con la otra interfaz de publicar objeto.

--- app/src/main/java/Vistas/MenuPrincipalActivity.java
+++ app/src/main/java/Vistas/MenuPrincipalActivity.java
@@ -1,6 +1,9 @@
 package Vistas;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,6 +24,16 @@ public class MenuPrincipalActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        Button btnTrueque = findViewById(R.id.btn_trueque);
+
+        btnTrueque.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MenuPrincipalActivity.this, PublicarObjetoActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
