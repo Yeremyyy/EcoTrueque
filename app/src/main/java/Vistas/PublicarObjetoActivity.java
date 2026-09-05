fc727d17a83ccb89199fbf810fdd385fe664fd6e
Yeremyyy <zackusskgg@gmail.com>
2026-09-05 10:35:20 -0400

    Sexto commit: Se impplemento la logica de la actividad de PublicarObjeto, se lee absolutamente cada parametro que se coloca en los input, y tambien un if para leer las posible categoria a seleccionar el usuario.

--- app/src/main/java/Vistas/PublicarObjetoActivity.java
+++ app/src/main/java/Vistas/PublicarObjetoActivity.java
@@ -1,6 +1,11 @@
 package Vistas;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,5 +27,38 @@ public class PublicarObjetoActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        EditText txtNombre = findViewById(R.id.txt_nombre);
+        RadioGroup rdgCategorias = findViewById(R.id.rdg_categorias);
+        Button btnPublicar = findViewById(R.id.btn_publicar);
+
+        btnPublicar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String nombreProducto = txtNombre.getText().toString();
+
+                if (nombreProducto.isEmpty()){
+                    txtNombre.setError("El nombre no puede estar vacio");
+                } else {
+                    String categoriaSeleccionada = "Sin categoria";
+                    int idCategoria = rdgCategorias.getCheckedRadioButtonId();
+
+                    if (idCategoria == R.id.rd_ropa){
+                        categoriaSeleccionada = "Ropa";
+                    } else if (idCategoria == R.id.rd_libros){
+                        categoriaSeleccionada = "Libros";
+                    } else if (idCategoria == R.id.rd_tecnologia){
+                        categoriaSeleccionada = "Tecnologia";
+                    }
+
+                    Intent intent = new Intent(PublicarObjetoActivity.this, DetalleObjetoActivity.class);
+                    intent.putExtra("EXTRA_NOMBRE", nombreProducto);
+                    intent.putExtra("EXTRA_CATEGORIA", categoriaSeleccionada);
+
+                    startActivity(intent);
+                }
+            }
+        });
+
     }
 }
