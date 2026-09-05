fcc313eb4e898b5fc1a1c94a97f6040d8939478b
Yeremyyy <zackusskgg@gmail.com>
2026-09-05 10:53:09 -0400

    Septimo commit: Se implemento la logica de la interfaz final de detalle objeto, en esta se mostrara el nombre y categoria del producto para posteriormente aceptar y ser retornado a la actividad del menu prinicpal, concluyendo todo el ciclo de la aplicacion. Se realizara algunas pruebas en este momento.

--- app/src/main/java/Vistas/DetalleObjetoActivity.java
+++ app/src/main/java/Vistas/DetalleObjetoActivity.java
@@ -1,6 +1,10 @@
 package Vistas;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,6 +25,32 @@ public class DetalleObjetoActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        TextView lblMostrarNombre = findViewById(R.id.lbl_nombre);
+        TextView lblMostrarCategoria = findViewById(R.id.lbl_categoriaprod);
+        Button btnAceptar = findViewById(R.id.btn_Aceptar);
+
+        Intent intentRecibido = getIntent();
+
+        String nombreRecibido = intentRecibido.getStringExtra("EXTRA_NOMBRE");
+        String categoriaRecibida = intentRecibido.getStringExtra("EXTRA_CATEGORIA");
+
+        if (nombreRecibido != null){
+            lblMostrarNombre.setText(nombreRecibido);
+        }
+        if(categoriaRecibida != null){
+            lblMostrarCategoria.setText(categoriaRecibida);
+        }
+
+        btnAceptar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentRetorno = new Intent(DetalleObjetoActivity.this, MenuPrincipalActivity.class);
+                intentRetorno.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+
+                startActivity(intentRetorno);
+            }
         });
     }
 }
